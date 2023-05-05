353558aa610d051254d4aa537627b4a6b1921003
Betico1928 <betoveluis@gmail.com>
2023-05-05 18:34:55 -0500

    Creación de clases

--- Taller de Mixins/src/main.scala
+++ Taller de Mixins/src/main.scala
@@ -1,4 +1,44 @@
+
+// Empleado
+class Empleado ( var nombre: String, var ingresoBase : Double)
+
+
+// Empleado de Planta
+class EmpleadoPlanta (var descuentosMes: Double)
+{
+  def PagoMensual(base: Double) : Double =
+  {
+    base * (descuentosMes) // kinda
+  }
+}
+
+
+// Contratista
+class Contratista (var reporteTiempo : Int)
+{
+  def PagoMensual(base: Double): Double =
+  {
+    base * (reporteTiempo) // kinda
+  }
+}
+
+
+// Vendedor Planta
+class VendedorPlanta (var reporteVentas : Double)
+
+
+// Vendedor Externo
+class VendedorExterno (var reporteVentas : Double)
+
+
+// Mixin
+trait reporteVentas
+{
+
+}
+
 @main
-def main(): Unit = {
+def main(): Unit =
+{
   println("Hello world!")
 }
